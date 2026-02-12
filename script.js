84134891cbc7d7adf6733b786adb0d09fccc35f0
temurshokirov14-lang <temurshokirov14@gmail.com>
2026-02-12 19:00:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let foydalanuvchiSozi = prompt("Ismingizni kiriting:");
+
+let ekranMatni = document.getElementById("matn");
+
+if (foydalanuvchiSozi != null) {
+    ekranMatni.innerText = foydalanuvchiSozi;
+}
